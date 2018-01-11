daf64d33d042a713bad995cd0505540fadef0b4c
laccey <1040664047@qq.com>
2018-01-11 18:03:49 +0800

    商家页面完成

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="name">{{seller.name}}</h1>
@@ -8,9 +8,6 @@
           <span class="count">({{seller.ratingCount}})</span>
           <span class="count">月售{{seller.sellCount}}单</span>
         </div>
-        <!--<div class="collect">-->
-          <!--<span class="icon-favorite"></span>-->
-        <!--</div>-->
         <ul class="remark">
           <li class="block">
             <h2>起送价</h2>
@@ -31,13 +28,41 @@
             </div>
           </li>
         </ul>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active':favorite}"></span>
+          <span class="text">{{favorteText}}</span>
+        </div>
       </div>
       <split></split>
       <div class="bulletin">
         <h1 class="title">公告与活动</h1>
-        <div class="content-wrapper">
+        <div class="content-wrapper border-1px">
           <p class="content">{{seller.bulletin}}</p>
         </div>
+        <ul v-if="seller.supports" class="supports">
+          <li class="support-item border-1px" v-for="item in seller.supports">
+            <span class="icon" :class="classMap[item.type]"></span>
+            <span class="text">{{item.description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="pic in seller.pics">
+              <img :src="pic" alt="" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item border-1px" v-for="info in seller.infos">{{info}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -46,6 +71,8 @@
 <script type="text/ecmascript-6">
   import star from '../star/star';
   import split from '../split/split';
+  import BScroll from 'better-scroll';
+  import {saveToLocal} from '../../common/js/store';
 
   export default {
     // name: 'seller'
@@ -54,9 +81,61 @@
         type: Object
       }
     },
+    data() {
+      return {
+        favorite: false
+      };
+    },
+    computed: {
+      favorteText() {
+        return this.favorite ? '已收藏' : '收藏';
+      }
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    methods: {
+      _initPic() {
+        if (this.seller.pics) {
+          let picWidth = 120;
+          let margin = 6;
+          let width = (picWidth + margin) * this.seller.pics.length - margin;
+          this.$refs.picList.style.width = width + 'px';
+          this.$nextTick(() => {
+            if (!this.picScroll) {
+              this.picScroll = new BScroll(this.$refs.picWrapper, {
+                scrollX: true,
+                eventPassthrough: 'vertical'
+              });
+            } else {
+              this.picScroll.refresh();
+            }
+          });
+        }
+      },
+      toggleFavorite() {
+        if (!event._constructed) {
+          return;
+        }
+        this.favorite = !this.favorite;
+        saveToLocal(this.seller.id, 'favorite', this.favorite);
+      }
+    },
+    watch: {
+      'seller'() {
+        this._initPic();
+      }
+    },
+    mounted() {
+      this.scroll = new BScroll(this.$refs.seller, {
+        click: true
+      });
+      this._initPic();
+    },
     components: {
       star,
-      split
+      split,
+      BScroll
     }
   };
 </script>
@@ -74,6 +153,25 @@
     .seller-content
       .overview
         padding:18px;
+        position:relative;
+        .favorite
+          position:absolute;
+          width:40px;
+          top:18px;
+          right:18px;
+          text-align:center;
+          .icon-favorite
+            display:block;
+            line-height:24px;
+            font-size:24px;
+            color:#d4d6d9;
+            margin-bottom:4px;
+            &.active
+              color:rgb(240,20,20);
+          .text
+            line-height:10px;
+            font-size:10px;
+            color:rgb(77,85,93);
         .name
           line-height:14px;
           font-size:14px;
@@ -114,6 +212,83 @@
               color:rgb(7,17,27);
               .stress
                 font-size:24px;
+
       .bulletin
-        margin-top:18px;
+        padding:18px 18px 0 18px;
+        .title
+          line-height:14px;
+          font-size:14px;
+          color:rgb(7,17,27);
+          margin-bottom:8px;
+        .content-wrapper
+          padding:0 12px 16px;
+          border-1px(rgba(7,17,27,0.1));
+          .content
+            line-height:24px;
+            font-size:12px;
+            color:rgb(240,20,20);
+        .supports
+          .support-item
+            border-1px(rgba(7,17,27,0.1));
+            padding:16px 12px;
+            font-size:0;
+            &:last-child
+              border-none();
+            .icon
+              display:inline-block;
+              vertical-align:top;
+              width:16px;
+              height:16px;
+              margin-right:6px;
+              background-size:100%;
+              &.decrease
+                bg-image('decrease_4');
+              &.discount
+                bg-image('discount_4');
+              &.guarantee
+                bg_image('guarantee_4');
+              &.invoice
+                bg-image('invoice_4');
+              &.special
+                bg-image('special_4');
+            .text
+              line-height:16px;
+              font-size:12px;
+              color:rgb(7,17,27);
+
+      .pics
+        padding:18px;
+        .title
+          line-height:14px;
+          font-size:14px;
+          color:rgb(7,17,27);
+          margin-bottom:12px;
+        .pic-wrapper
+          width:100%;
+          overflow:hidden;
+          white-space:nowrap;
+          .pic-list
+            font-size:0;
+            .pic-item
+              display:inline-block;
+              margin-right:6px;
+              width:120px;
+              height:90px;
+              &:last-child
+                margin-right:0;
+      .info
+        padding:18px 18px 0 18px;
+        color:rgb(7,17,27);
+        .title
+          line-height:14px;
+          font-size:14px;
+          padding-bottom:12px;
+          border-1px(rgba(7,17,27,0.1));
+        .info-item
+          padding:16px 12px;
+          font-size:12px;
+          line-height:16px;
+          border-1px(rgba(7,17,27,0.1));
+          &:last-child
+            border-none();
 </style>
